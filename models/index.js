dcd8dabf651f3c92cda777186facd241483acc09
zoe <zoeedge0703@gmail.com>
2023-07-31 12:59:27 -0500

    added the relationships between Post and Comment

--- models/index.js
+++ models/index.js
@@ -5,7 +5,13 @@ const SignUp = require('./SignUp');
 
 
 // use this file to connect the directory to your other files.
-// Blog post is most important
+// Post is most important
 // Post hasMany Comment
+Post.hasMany(Comment, {
+    foreignKey: 'comment_id'
+});
 // Comment belongsTo Post
+Comment.belongsTo(Post, {
+    foreignKey: 'comment_id'
+});
 // unsure of how SignUp and SignIn work in this context above

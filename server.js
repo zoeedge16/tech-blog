254cdc0b0df5794f350983ff49eef01becfb9192
zoe <zoeedge0703@gmail.com>
2023-07-31 18:04:22 -0500

    setting up the server.js to authenticate users

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const sequilize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const hbs = exphbs.create();
@@ -14,6 +16,19 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(
+    session({
+        secret: 'SweetPeachPie12:)',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000,
+        }
+    })
+)
+
+// setting up authentication routes and using bcrypt library to hash passwords and manage user authentication
+
 
 sequilize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {

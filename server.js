fd99c3ee6a2d2d93d38d53799f0bdda335ae238d
zoe <zoeedge0703@gmail.com>
2023-07-31 16:17:32 -0500

    setting up the server.js to use handlebars

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const sequilize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const app = express();
+const hbs = exphbs.create();
 const PORT = process.env.PORT ?? 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // middleware
 app.use(express.json());
